4d84c8ea641b2d85e3821745903a91739848f703
Jior <JamesIoriattiII2023@u.northwestern.edu>
2023-01-17 18:17:18 -0600

    Reformatted front page per assignemnt req's, added post route to view post details.

--- routes/homeRoutes.js
+++ routes/homeRoutes.js
@@ -6,12 +6,12 @@ const {Post, Comment, User} = require('../models');
 
 
 
-const userName = async (id) =>{
-    const name = await User.findOne({
-        where :{id: id},
-    },)
-    return name.dataValues.username;
-}
+// const userName = async (id) =>{
+//     const name = await User.findOne({
+//         where :{id: id},
+//     },)
+//     return name.dataValues.username;
+// }
 // '/' base routes
 router.get('/', async (req, res) => {
     try{
@@ -43,6 +43,37 @@ router.get('/', async (req, res) => {
     }
 });
 
+//sepcific post route
+router.get('/posts/:id', async (req, res)=>{
+    try{
+        const postData = await Post.findByPk(req.params.id,{
+            include: [{
+                model: User,
+                attributes: [
+                    'id','username'
+                ]
+            },
+            {
+                model: Comment,
+                attributes: [
+                    'id', 'content', 'creator_id', 'createdAt', 'updatedAt'
+                ],
+                include: [{
+                    model: User,
+                    attributes: [
+                        'id', 'username'
+                    ]
+                }]
+            }]
+        });
+        const post = postData.get({plain:true});
+        console.log(post);
+        res.render('post', post);
+    }catch(err){
+        res.status(500).json(err);
+    }
+
+})
 
 
 module.exports = router;
