1b5eb983593931daad81a031ff3b5865ba5752d3
Jior <JamesIoriattiII2023@u.northwestern.edu>
2023-02-09 20:06:53 -0600

    Added in foreign key constraint workaround for seeding DB

--- seeds/index.js
+++ seeds/index.js
@@ -5,7 +5,9 @@ const seedComments = require('./commentSeeds');
 const sequelize = require('../config/connection');
 
 const seedDB = async () =>{
-    await sequelize.sync({force:true});
+    await sequelize.query("SET FOREIGN_KEY_CHECKS = 0")
+    await sequelize.sync({force: true})
+    await sequelize.query("SET FOREIGN_KEY_CHECKS = 1")
     console.log("\n**** Database Synced ****");
     await seedUsers();
     console.log("\n**** Users Seeded ****");
